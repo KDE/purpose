20a304236100b29014403a894cbda2d93ab28e41
Aleix Pol <aleixpol@kde.org>
2018-01-21 20:34:07 +0100

    Fix applications linking against KDEExperimental:: targets
    Copy the targets

--- KDEExperimentalPurposeConfig.cmake
+++ KDEExperimentalPurposeConfig.cmake
@@ -1,2 +1,19 @@
 find_package(KF5Purpose REQUIRED)
 message(WARNING "Look up the Purpose framework using KF5Purpose")
+
+set(BT ${CMAKE_BUILD_TYPE})
+string(TOUPPER ${BT} BT)
+
+set(props INTERFACE_INCLUDE_DIRECTORIES INTERFACE_LINK_LIBRARIES IMPORTED_LINK_DEPENDENT_LIBRARIES_${BT} IMPORTED_LOCATION_${BT} IMPORTED_SONAME_${BT})
+
+function(_copyTarget A B)
+    add_library(${B} SHARED IMPORTED)
+    foreach(prop ${props})
+        get_target_property(value ${A} ${prop})
+        message(STATUS "set_target_properties(${B} PROPERTIES ${prop} ${value})")
+        set_target_properties(${B} PROPERTIES ${prop} "${value}")
+    endforeach()
+endfunction()
+
+_copyTarget(KF5::Purpose KDEExperimental::Purpose)
+_copyTarget(KF5::PurposeWidgets KDEExperimental::PurposeWidgets)
