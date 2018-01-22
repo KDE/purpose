000fb91b0bfdac1291d53a7b9d0155cd46f95664
Aleix Pol <aleixpol@kde.org>
2018-01-22 17:24:35 +0100

    Make sure we're importing all configurations for KF5::Purpose

--- KDEExperimentalPurposeConfig.cmake
+++ KDEExperimentalPurposeConfig.cmake
@@ -1,16 +1,21 @@
 find_package(KF5Purpose REQUIRED)
 message(WARNING "Look up the Purpose framework using KF5Purpose")
 
-set(BT ${CMAKE_BUILD_TYPE})
-string(TOUPPER ${BT} BT)
-
-set(props INTERFACE_INCLUDE_DIRECTORIES INTERFACE_LINK_LIBRARIES IMPORTED_LINK_DEPENDENT_LIBRARIES_${BT} IMPORTED_LOCATION_${BT} IMPORTED_SONAME_${BT})
-
 function(_copyTarget A B)
     add_library(${B} SHARED IMPORTED)
+
+    set(props INTERFACE_INCLUDE_DIRECTORIES INTERFACE_LINK_LIBRARIES IMPORTED_CONFIGURATIONS)
+
+    get_target_property(value ${A} IMPORTED_CONFIGURATIONS)
+    foreach(cfg ${value})
+        list(APPEND props IMPORTED_LINK_DEPENDENT_LIBRARIES_${cfg} IMPORTED_LOCATION_${cfg} IMPORTED_SONAME_${cfg})
+    endforeach()
+
     foreach(prop ${props})
         get_target_property(value ${A} ${prop})
-        message(STATUS "set_target_properties(${B} PROPERTIES ${prop} ${value})")
+        if (NOT value)
+            message(WARNING "missing property ${A} ${prop}")
+        endif()
         set_target_properties(${B} PROPERTIES ${prop} "${value}")
     endforeach()
 endfunction()
