36e804a4b2cadc96858c98973065e1637c4d50f1
Nicolas Fella <nicolas.fella@gmx.de>
2020-08-28 12:34:49 +0200

    [fileitemactionplugin] Don't leak menu
    We cannot bind the menu lifetime to the lifetime of the plugin unfortunately since that is destroyed when the Dolphin context menu closes, but the menu needs to outlive it to handle the user interaction that follows.
    
    Instead delete the menu when it's finished.

--- src/fileitemactionplugin/sharefileitemaction.cpp
+++ src/fileitemactionplugin/sharefileitemaction.cpp
@@ -31,14 +31,15 @@ K_PLUGIN_CLASS_WITH_JSON(ShareFileItemAction, "sharefileitemaction.json")
 Q_LOGGING_CATEGORY(PURPOSE_FILEITEMACTION, "kf.kio.widgets.fileitemactions.purpose")
 
 ShareFileItemAction::ShareFileItemAction(QObject* parent, const QVariantList& )
-    : KAbstractFileItemActionPlugin(parent),
-    m_menu(new Purpose::Menu())
+    : KAbstractFileItemActionPlugin(parent)
 {
-    m_menu->setTitle(i18n("Share"));
-    m_menu->setIcon(QIcon::fromTheme(QStringLiteral("document-share")));
-    m_menu->model()->setPluginType(QStringLiteral("Export"));
+    Purpose::Menu *menu = new Purpose::Menu;
 
-    QObject::connect(m_menu, &Purpose::Menu::finished, [](const QJsonObject &output, int error, const QString &errorMessage) {
+    menu->setTitle(i18n("Share"));
+    menu->setIcon(QIcon::fromTheme(QStringLiteral("document-share")));
+    menu->model()->setPluginType(QStringLiteral("Export"));
+
+    QObject::connect(menu, &Purpose::Menu::finished, menu, [menu](const QJsonObject &output, int error, const QString &errorMessage) {
         if (error == 0 || error == KIO::ERR_USER_CANCELED) {
             if (output.contains(QLatin1String("url")))
                 QDesktopServices::openUrl(QUrl(output.value(QLatin1String("url")).toString()));
@@ -46,7 +47,11 @@ ShareFileItemAction::ShareFileItemAction(QObject* parent, const QVariantList& )
             KNotification::event(KNotification::Error, i18n("Error sharing"), errorMessage);
             qWarning() << "job failed with error" << error << errorMessage << output;
         }
+
+        menu->deleteLater();
     });
+
+    m_menu = menu;
 }
 
 QList<QAction*> ShareFileItemAction::actions(const KFileItemListProperties& fileItemInfos, QWidget* parentWidget)
