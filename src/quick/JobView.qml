KDE/purpose

commit 216ecc5214991ae52b78850ecb177d536286c8d4

Improve job window buttons
1. Use more relevant text
2. Give them icons
3. Correctly disable the Send button when no device is selected

BUG: 444145
FIXED-IN: 5.88

src/quick/JobView.qml
@@ -109,18 +109,27 @@ Item {
                     : ""
             }
 
+            // Not using a DialogButtonBox because it doesn't let us customize
+            // the buttons and conditionally disable any of them, which we want
             RowLayout {
                 Layout.leftMargin: Kirigami.Units.largeSpacing
                 Layout.rightMargin: Kirigami.Units.largeSpacing
                 Layout.bottomMargin: Kirigami.Units.largeSpacing
+                Layout.alignment: Qt.AlignRight
+
                 Button {
-                    text: i18nd("libpurpose_quick", "Run")
-                    enabled: jobController.configuration && jobController.configuration.isReady
+                    text: i18nd("libpurpose_quick", "Send")
+                    icon.name: "document-send"
+                    enabled: jobController.configuration
+                             && jobController.configuration.isReady
+                             && configLoader.item
+                             && configLoader.item.device != undefined
                     onClicked: jobController.startJob()
                 }
 
                 Button {
-                    text: i18nd("libpurpose_quick", "Back")
+                    text: i18nd("libpurpose_quick", "Cancel")
+                    icon.name: "dialog-cancel"
                     onClicked: jobController.cancel()
                 }
             }

src/plugins/bluetooth/bluetoothplugin_config.qml
@@ -38,6 +38,9 @@ ColumnLayout {
         ListView {
             id: list
 
+            // Don't select anything by default; make the user choose explicitly
+            currentIndex: -1
+
             clip: true
             model: BluezQt.DevicesModel { }
 

src/plugins/kdeconnect/kdeconnectplugin_config.qml
@@ -37,6 +37,9 @@ ColumnLayout {
         ListView {
             id: list
 
+            // Don't select anything by default; make the user choose explicitly
+            currentIndex: -1
+
             clip: true
             model: DevicesModel {
                 id: connectDeviceModel

commit 6973356b732ef540baa7924481dc62d784b4d503

Unbreak the Nextcloud plugin
Not every plugin config has a device property so binding the enabled status of the next button to it is wrong

It's also not needed since the config is not ready when no device is selected

This regression was introduced in 216ecc5214991ae52b78850ecb177d536286c8d4

src/quick/JobView.qml
@@ -122,8 +122,6 @@ Item {
                     icon.name: "document-send"
                     enabled: jobController.configuration
                              && jobController.configuration.isReady
-                             && configLoader.item
-                             && configLoader.item.device != undefined
                     onClicked: jobController.startJob()
                 }
 